6099c360d071aa705d4e421fdb2cde2642ddef72
Valmir Jr <valmirmassoni@gmail.com>
2017-11-22 18:14:44 -0200

    tap to play overlay and starand start mute and unmute music and song

---
+++ SpicyHero/SettingsController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsController.swift
+//  SpicyHero
+//
+//  Created by Valmir Junior on 22/11/17.
+//  Copyright © 2017 Valmir Junior. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+import GameKit
+//
+//protocol SettingsProtocol {
+//    func turnOnMusic()
+//}
+//
+//class SettingsController: SettingsProtocol {
+//    
+//    
+//}
+
