011662dad07dbf5028a3fe5ebcef35985fc9779e
Richard Vaz da Silva Netto <kenanrd@gmail.com>
2017-12-13 14:13:35 -0200

    No meio do Tutorial

---
+++ PepperJourney/GameState/TutorialFase1State.swift
@@ -0,0 +1,9 @@
+//
+//  TutorialFase1State.swift
+//  PepperJourney
+//
+//  Created by Richard Vaz da Silva Netto on 12/12/17.
+//  Copyright © 2017 Valmir Junior. All rights reserved.
+//
+
+import Foundation
