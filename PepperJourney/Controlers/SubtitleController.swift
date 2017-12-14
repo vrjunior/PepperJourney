ab8d87c27ca887ddf4c6c012f913631723e03e70
Marcelove <martimiano.marcelo@gmail.com>
2017-12-14 00:40:07 -0200

    legendas funcionais

---
+++ PepperJourney/Controlers/SubtitleController.swift
@@ -0,0 +1,9 @@
+//
+//  SubtitleController.swift
+//  PepperJourney
+//
+//  Created by Marcelo Martimiano Junior on 13/12/17.
+//  Copyright © 2017 Valmir Junior. All rights reserved.
+//
+
+import Foundation
