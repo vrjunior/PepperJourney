5dc9c56feb5df5a25f32bc1c89e1cf290e100257
Valmir Jr <valmirmassoni@gmail.com>
2018-01-03 17:10:47 -0200

    fix to release

--- PepperJourney/GameController.swift
+++ PepperJourney/GameController.swift
@@ -160,7 +160,7 @@ class GameController: NSObject, SCNSceneRendererDelegate, GameOptions {
         sceneRenderer!.delegate = self
         
         //self.scnView.debugOptions = SCNDebugOptions.showPhysicsShapes
-        self.scnView.showsStatistics = true
+        //self.scnView.showsStatistics = true
         
         self.addNotifications()
     }
